9728e13f02a90059b470ca27982bf65ca82686e9
Fukayanegi <fukayanegi1981@gmail.com>
2015-09-06 07:26:17 +0900

    add header comment to Q2.1rb

--- chapter2/Q2.1.rb
+++ chapter2/Q2.1.rb
@@ -1,3 +1,11 @@
+# -*- encoding: utf-8 -*-
+# 
+# ==Code for CodingInterview Chapter2 Q2.1 
+# 
+# Author:: taro-tz
+# Version:: 0.0.1
+# License:: MIT License
+
 puts "This is Q2.1.rb"
 
 list = MyList.new
